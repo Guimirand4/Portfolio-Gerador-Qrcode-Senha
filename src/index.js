0b352fe4f8d70cd458b34d4f979e96db4d82d374
Guimirand4 <guilhermemiranda419@gmail.com>
2025-07-04 18:20:13 -0300

    feat: opção para gerar qrcode no terminal funcionando

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import prompt from 'prompt';
-
-
 import mainPrompt from './prompts/prompt-main.js';
+import createQRCode from './services/qr-code/create.js';
 
 async function main() { 
  prompt.get(mainPrompt, async (err, choose) => {
     if(choose.select == 1) {
         console.log("Você escolheu a ferramenta QRCODE");
+        await createQRCode();   
 
     }
        if(choose.select == 2) {
---
+++ src/services/qr-code/handle.js
@@ -0,0 +1,18 @@
+import qr from "qrcode-terminal";
+import chalk from "chalk";
+
+async function handle(err, result){
+    if(err) {
+        console.error("An error occurred:", err);
+        return;
+    }
+
+    const isSmall = result.type == 2;
+    qr.generate(result.link, {small: isSmall}, (qrcode) => {
+        if(isSmall) {
+            console.log(qrcode);
+        }
+    });
+}
+
+export default handle;
